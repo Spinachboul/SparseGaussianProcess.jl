d0d8a1bf07b6aec34f90f8a16b545e62d4194391
Mridul Jain <jain.mridul.20@gmail.com>
2024-04-19 11:06:25 +0530

    yo

--- src/SparseGaussianProcess.jl
+++ src/SparseGaussianProcess.jl
@@ -2,4 +2,8 @@ module SparseGaussianProcess
 
 # Write your package code here.
 
+include("sample.jl")
+
+export my_f
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using SparseGaussianProcess
 using Test
 
+
+
 @testset "SparseGaussianProcess.jl" begin
-    # Write your tests here.
+   @test my_f(2,1) == 7
+   @test my_f(3,2) == 12
 end
