94cc00d4d82cb43f718b50f60898bd1cd81c105c
Mridul Jain <jain.mridul.20@gmail.com>
2024-04-30 01:53:32 +0530

    Add Julia environment path to .vscode/settings.json and update dependencies in Project.toml

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
-using SparseGaussianProcess
 using Test
+using SparseGaussianProcess  # Import your package module
 
-
-
-@testset "SparseGaussianProcess.jl" begin
-   @test my_f(2,1) == 7
-   @test my_f(3,2) == 12
-end
+# Test set_inducing_inputs!
+sgp = SparseGaussianProcess.SGP()
+X = rand(10, 2)  # Example training data
+SparseGaussianProcess.set_inducing_inputs!(sgp, X)
+@test !isnothing(sgp.Z)
+@test size(sgp.Z, 2) == size(X, 2)
+@test sgp.nz == sgp.options["n_inducing"]  # Check if number of inducing inputs is correct
