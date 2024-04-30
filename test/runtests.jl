20f6865c430e6277d9e2d2806d6e3cb7ddb8c563
MRIDUL JAIN <105979087+Spinachboul@users.noreply.github.com>
2024-04-30 15:34:50 +0530

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,15 +4,10 @@ using SparseGaussianProcess  # Import your package module
 # Test set_inducing_inputs!
 @testset "set_inducing_inputs!" begin
     sgp = SparseGaussianProcess.SGP()
-    X = rand(10, 2)  # Example training data
+    X = rand(10,2)  # Example training data
     SparseGaussianProcess.set_inducing_inputs!(sgp, X)
 
     @test !isnothing(sgp.Z)
     @test size(sgp.Z, 2) == size(X, 2)
     @test sgp.nz == sgp.options["n_inducing"]
-
-
-@testset "SparseGaussianProcess.jl" begin
-   @test my_f(2,1) == 7
-   @test my_f(3,2) == 12
 end
